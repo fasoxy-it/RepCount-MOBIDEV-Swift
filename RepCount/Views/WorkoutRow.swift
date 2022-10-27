729336c54c780dfa3d5164c7f58ccf0f9f6869f3
Mattia Fasoli <fasoxy@gmail.com>
2022-10-27 17:24:42 +0200

    Create WorkoutRow file

---
+++ RepCount/Views/WorkoutRow.swift
@@ -0,0 +1,20 @@
+//
+//  WorkoutRow.swift
+//  RepCount
+//
+//  Created by Mattia Fasoli on 27/10/22.
+//
+
+import SwiftUI
+
+struct WorkoutRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WorkoutRow_Previews: PreviewProvider {
+    static var previews: some View {
+        WorkoutRow()
+    }
+}
