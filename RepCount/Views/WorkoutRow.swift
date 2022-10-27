a31fbba13edefa55556fcf71810167eeadeb4d85
Mattia Fasoli <fasoxy@gmail.com>
2022-10-27 17:28:39 +0200

    Modify WorkoutRow file

--- RepCount/Views/WorkoutRow.swift
+++ RepCount/Views/WorkoutRow.swift
@@ -8,13 +8,50 @@
 import SwiftUI
 
 struct WorkoutRow: View {
+    
+    var workout: Workout
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.black
+                .ignoresSafeArea()
+            HStack {
+                VStack {
+                    ZStack {
+                        Circle()
+                            .foregroundColor(.gray)
+                            .frame(width: 75, height: 75)
+                        Image(systemName: workout.logo)
+                            .font(.system(size: 34))
+                            .foregroundColor(.green)
+                    }
+                }
+                VStack {
+                    HStack {
+                        Text(workout.name)
+                            .font(.title3)
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                        Spacer()
+                    }
+                    HStack {
+                        Text(workout.muscle)
+                            .font(.body)
+                            .foregroundColor(.white)
+                        Spacer()
+                    }
+                }
+            }
+        }
     }
 }
 
 struct WorkoutRow_Previews: PreviewProvider {
     static var previews: some View {
-        WorkoutRow()
+        Group {
+            WorkoutRow(workout: Workouts().workouts[0])
+            WorkoutRow(workout: Workouts().workouts[1])
+            WorkoutRow(workout: Workouts().workouts[2])
+        }
     }
 }
