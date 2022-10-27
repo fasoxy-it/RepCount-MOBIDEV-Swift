948fcad873fe5ffe803f439a411ca0908aa81f25
Mattia Fasoli <fasoxy@gmail.com>
2022-10-27 17:39:32 +0200

    Create WorkoutDetail file

---
+++ RepCount/Views/WorkoutDetail.swift
@@ -0,0 +1,20 @@
+//
+//  WorkoutDetail.swift
+//  RepCount
+//
+//  Created by Mattia Fasoli on 27/10/22.
+//
+
+import SwiftUI
+
+struct WorkoutDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WorkoutDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        WorkoutDetail()
+    }
+}
