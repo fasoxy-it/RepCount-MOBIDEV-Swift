c29d63e9bb83b2a6b5c74ef299e61fbea4bc5ef6
Mattia Fasoli <fasoxy@gmail.com>
2022-10-27 17:29:29 +0200

    Create WorkoutList file

---
+++ RepCount/Views/WorkoutList.swift
@@ -0,0 +1,20 @@
+//
+//  WorkoutList.swift
+//  RepCount
+//
+//  Created by Mattia Fasoli on 27/10/22.
+//
+
+import SwiftUI
+
+struct WorkoutList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WorkoutList_Previews: PreviewProvider {
+    static var previews: some View {
+        WorkoutList()
+    }
+}
