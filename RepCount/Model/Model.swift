7ab405ad04923aea7a69de4e1d302c72d56801f2
Mattia Fasoli <fasoxy@gmail.com>
2022-10-27 17:19:50 +0200

    Add Workout struct to Model

--- RepCount/Model/Model.swift
+++ RepCount/Model/Model.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+struct Workout {
+    var name: String
+    var logo: String
+    var description: String
+    var video: String
+    var muscle: String
+    var intensity: String
+    var attentions: [String]
+}
