be5f1e4a98f0df978c2db6dd3359d55a91ba2c93
Mattia Fasoli <fasoxy@gmail.com>
2022-11-11 19:18:58 +0100

    Modify

--- RepCount/Views/WorkoutControl.swift
+++ RepCount/Views/WorkoutControl.swift
@@ -9,7 +9,15 @@ import SwiftUI
 import RealityKit
 import AVFoundation
 
+class ViewModel: ObservableObject {
+    @Published var label: String = ""
+    @Published var count: Double = 0.0
+    @Published var countMistake: Double = 0.0
+}
+
 struct WorkoutControl: View {
+    
+    @ObservedObject var viewModel = ViewModel()
     
     @State var isTimerRunning: Bool = true
     @State var timerCount: Double = 0.0
@@ -23,14 +31,8 @@ struct WorkoutControl: View {
         
         NavigationView {
             ZStack {
-                ViewWrapper()
+                ViewWrapper(viewModel: viewModel)
                     .edgesIgnoringSafeArea(.all)
-                //GeometryReader { geo in
-                //    CameraViewWrapper(poseEstimator: poseEstimator)
-                //    StickFigureView(poseEstimator: poseEstimator, size: geo.size)
-                //}
-                //ARViewContainer(arViewModel: arViewModel)
-                    //.edgesIgnoringSafeArea(.all)
                 VStack {
                     Spacer()
                     HStack {
@@ -41,7 +43,8 @@ struct WorkoutControl: View {
                                 .frame(height: 180)
                             VStack {
                                 HStack {
-                                    Text(workout.name)
+                                    Text(viewModel.label)
+                                    //Text(workout.name)
                                         .font(.title)
                                         .fontWeight(.bold)
                                         .foregroundColor(.white)
@@ -65,7 +68,8 @@ struct WorkoutControl: View {
                                     Image(systemName: "arrow.clockwise.circle.fill")
                                         .font(.system(size: 22))
                                         .foregroundColor(Color("Green"))
-                                    Text("00")
+                                    Text(String(format: "%.0f", viewModel.count))
+                                    //Text("00")
                                         .font(.title3)
                                         .foregroundColor(Color("Green"))
                                     Spacer()
@@ -74,7 +78,8 @@ struct WorkoutControl: View {
                                     Image(systemName: "exclamationmark.circle.fill")
                                         .font(.system(size: 22))
                                         .foregroundColor(Color("Red"))
-                                    Text("00")
+                                    Text(String(format: "%.0f", viewModel.countMistake))
+                                    //Text("00")
                                         .font(.title3)
                                         .foregroundColor(Color("Red"))
                                     Spacer()
@@ -118,7 +123,7 @@ struct WorkoutControl: View {
                                 }.padding(.bottom, 5)
                                 HStack {
                                     Spacer()
-                                    NavigationLink(destination: WorkoutSummary(workout: workout, timeing: timerCount, repetitions: 25, mistakes: 0)) {
+                                    NavigationLink(destination: WorkoutSummary(workout: workout, timeing: timerCount, repetitions: Int(viewModel.count), mistakes: Int(viewModel.countMistake))) {
                                         Image(systemName: "x.circle.fill")
                                             .font(.system(size: 38))
                                             .foregroundColor(.white)
@@ -150,4 +155,151 @@ func formatTimerMmSsMSms(counter: Double) -> String {
     let seconds = Int(counter) % 60
     let milliseconds = Int(counter*1000) % 1000
     return String(format: "%02d:%02d:%02d", minutes, seconds, milliseconds)
+}
+
+class ViewController: UIViewController {
+    
+    var viewModel: ViewModel?
+    
+    private var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return imageView
+    }()
+    
+    var videoCapture: VideoCapture!
+    var videoProcessingChain: VideoProcessingChain!
+    var actionFrameCounts = [String: Int]()
+    var actionRepCounts = [String: Int]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        videoProcessingChain = VideoProcessingChain()
+        videoProcessingChain.delegate = self
+        
+        videoCapture = VideoCapture()
+        videoCapture.delegate = self
+        
+        view.addSubview(imageView)
+        NSLayoutConstraint.activate([
+            imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            imageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+            imageView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0),
+        ])
+        
+    }
+    
+}
+
+extension ViewController: VideoCaptureDelegate {
+    
+    func videoCapture(_ videoCapture: VideoCapture, didCreate framePublisher: FramePublisher) {
+        updateUILabelsWithPrediction(.startingPrediction)
+        videoProcessingChain.upstreamFramePublisher = framePublisher
+    }
+    
+}
+
+extension ViewController: VideoProcessingChainDelegate {
+    
+    func videoProcessingChain(_ chain: VideoProcessingChain, didPredict actionPrediction: ActionPrediction, for frameCount: Int) {
+        if actionPrediction.isModelLabel {
+            //addFrameCount(frameCount, to: actionPrediction.label)
+            addRepCount(frameCount, to: actionPrediction.label)
+            //addRepCount(1, to: actionPrediction.label)
+        }
+        updateUILabelsWithPrediction(actionPrediction)
+    }
+    
+    func videoProcessingChain(_ chain: VideoProcessingChain, didDetect poses: [Pose]?, in frame: CGImage) {
+        DispatchQueue.global(qos: .userInteractive).async {
+            self.drawPoses(poses, onto: frame)
+        }
+    }
+    
+}
+
+extension ViewController {
+    
+    private func addRepCount(_ repCount: Int, to actionLabel: String) {
+        let totalReps = (actionRepCounts[actionLabel] ?? 0) + repCount
+        actionRepCounts[actionLabel] = totalReps
+        
+        if actionLabel == "Jumping Jacks" {
+            self.viewModel?.count = (Double(totalReps) / 40)
+            //self.viewModel?.count = (Double(totalReps) / ExerciseClassifier.frameRate)
+        } else if actionLabel == "Lunges" {
+            self.viewModel?.countMistake = (Double(totalReps) / 55)
+            //self.viewModel?.countMistake = (Double(totalReps) / ExerciseClassifier.frameRate)
+        }
+        
+    }
+    
+    /*
+    private func addFrameCount(_ frameCount: Int, to actionLabel: String) {
+        let totalFrames = (actionFrameCounts[actionLabel] ?? 0) + frameCount
+        actionFrameCounts[actionLabel] = totalFrames
+    }
+    */
+    
+    private func updateUILabelsWithPrediction(_ prediction: ActionPrediction) {
+        DispatchQueue.main.async {
+            if prediction.label == "Jumping Jacks" {
+                self.viewModel?.label = prediction.label
+            } else {
+                self.viewModel?.label = "Different"
+            }
+        }
+    }
+    
+    private func drawPoses(_ poses: [Pose]?, onto frame: CGImage) {
+        
+        let renderFormat = UIGraphicsImageRendererFormat()
+        renderFormat.scale = 1.0
+        
+        let frameSize = CGSize(width: frame.width, height: frame.height)
+        let poseRender = UIGraphicsImageRenderer(size: frameSize, format: renderFormat)
+        
+        let frameWithPoseRendering = poseRender.image {
+            renderContext in
+            
+            let cgContext = renderContext.cgContext
+            let inverse = cgContext.ctm.inverted()
+            
+            cgContext.concatenate(inverse)
+            
+            let imageRectangle = CGRect(origin: .zero, size: frameSize)
+            cgContext.draw(frame, in: imageRectangle)
+            
+            let pointTransform = CGAffineTransform(scaleX: frameSize.width, y: frameSize.height)
+            
+            guard let poses = poses else { return }
+            
+            for pose in poses {
+                pose.drawWireframeToContext(cgContext, applying: pointTransform)
+            }
+        }
+        DispatchQueue.main.async {
+            self.imageView.image = frameWithPoseRendering
+        }
+    }
+    
+}
+
+struct ViewWrapper: UIViewControllerRepresentable {
+    
+    var viewModel: ViewModel
+    
+    func makeUIViewController(context: Context) -> ViewController {
+        let mvc = ViewController()
+        mvc.viewModel = viewModel
+        return mvc
+    }
+    
+    func updateUIViewController(_ uiViewController: ViewController, context: Context) {}
+    
 }
